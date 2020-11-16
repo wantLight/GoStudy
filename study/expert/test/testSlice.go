40ed9a484a42b8d10de4bcce14b73ec45233462c
xyzzg <1364932323@qq.com>
2020-11-16 18:35:42 +0800

    如果切片的容量小于1024个元素，那么扩容的时候slice的cap就翻番，乘以2

---
+++ study/expert/test/testSlice.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/**
+如果切片的容量小于1024个元素，那么扩容的时候slice的cap就翻番，乘以2；一旦元素个数超过1024个元素，增长因子就变成1.25，即每次增加原来容量的四分之一。
+如果扩容之后，还没有触及原数组的容量，那么，切片中的指针指向的位置，就还是原数组，如果扩容之后，超过了原数组的容量，那么，Go就会开辟一块新的内存，把原来的值拷贝过来，这种情况丝毫不会影响到原数组。
+*/
+func AddElement(slice []int, e int) []int {
+	return append(slice, e)
+}
+func main() {
+	var slice []int
+	fmt.Println("容量：", cap(slice), " 长度", len(slice))
+	slice = append(slice, 1, 2, 3)
+	fmt.Println("容量：", cap(slice), " 长度", len(slice))
+	newSlice := AddElement(slice, 4)
+	fmt.Println("容量：", cap(newSlice), " 长度", len(newSlice))
+	fmt.Println(&slice[0] == &newSlice[0])
+}
--- study/function/GoChanTest.go
+++ study/function/GoChanTest.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func sum1(s []int, c chan int) {
 	sum := 0
@@ -10,14 +13,33 @@ func sum1(s []int, c chan int) {
 	c <- sum // 将和送入 c
 }
 
+func myRange(mychan chan int) {
+	for v := range mychan {
+		fmt.Println(v)
+	}
+}
+
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
+	//s := []int{7, 2, 8, -9, 4, 0}
+	//
+	//c := make(chan int)
+	//go sum1(s[:len(s)/2], c)
+	//go sum1(s[len(s)/2:], c)
+	//go sum1([]int{1, 2, 3}, c)
+	//// 从 c 中接收
+	//x, y, z := <-c, <-c, <-c
+	//fmt.Println(x, y, z, x+y)
 
-	c := make(chan int)
-	go sum1(s[:len(s)/2], c)
-	go sum1(s[len(s)/2:], c)
-	go sum1([]int{1, 2, 3}, c)
-	x, y, z := <-c, <-c, <-c // 从 c 中接收
+	samChan := make(chan int, 3)
+	go myRange(samChan)
+	samChan <- 8
+	samChan <- 7
+	samChan <- 6
+	samChan <- 5
+	samChan <- 4
+	// 在生产者的地方关闭channel
+	// 如果向此channel写数据的goroutine退出时，系统检测到这种情况后会panic，否则range将会永久阻塞。
+	close(samChan)
+	time.Sleep(time.Second * 10) // 正常
 
-	fmt.Println(x, y, z, x+y)
 }
